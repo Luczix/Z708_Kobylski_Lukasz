c47ecccce0fc6b5ede2a0d69a5890df7d38f58e6
Luczix <97066214+Luczix@users.noreply.github.com>
2022-01-03 23:44:53 +0100

    Add files via upload

---
+++ Jachty/Jachty/Models/Jacht.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Jachty.Models
+{
+    public class Jacht
+    {
+        public int id { get; set; }
+        public string Nazwa { get; set; }
+        public string Rodzaj { get; set; }
+        public int Cena_Jedn { get; set; }
+
+
+    }
+}
